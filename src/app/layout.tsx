b7422652b9da1817a1ed7be097627f2e005eda56
Vusal <iamvusalalakbarov@gmail.com>
2026-01-21 18:15:14 +0400

    refactor: made adjustments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
+import { PropsWithChildren } from "react";
 import { Roboto } from "next/font/google";
 import { ReactQueryProvider } from "@/providers/ReactQueryProvider";
-import "./globals.css";
+import { Header } from "@/components/Header";
+import "../styles/globals.css";
 
 const roboto = Roboto({
   variable: "--font-roboto",
@@ -14,13 +16,16 @@ export const metadata: Metadata = {
     "The goal of this project is to use ReactJS capabilities and its paradigms by creating a simple Crypto Trading App.",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
   return (
     <html lang="en">
-      <body className={`${roboto.variable} antialiased`}>
-        <ReactQueryProvider>{children}</ReactQueryProvider>
+      <body
+        className={`${roboto.variable} text-slate-50 bg-slate-950 antialiased`}
+      >
+        <ReactQueryProvider>
+          <Header />
+          <main className="p-8">{children}</main>
+        </ReactQueryProvider>
       </body>
     </html>
   );
