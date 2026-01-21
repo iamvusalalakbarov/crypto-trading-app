7dc1e35af7984f263f9dc67549f7ec732855512f
Vusal <iamvusalalakbarov@gmail.com>
2026-01-21 18:15:26 +0400

    feat: added constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,14 @@
+export const NAV_LINKS = [
+  {
+    id: 1,
+    name: "Home",
+    href: "/",
+  },
+  {
+    id: 2,
+    name: "Trade",
+    href: "/trade",
+  },
+];
+
+export const ASSETS_PER_PAGE = 10;
