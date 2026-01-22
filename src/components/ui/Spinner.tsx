6c2c375c548418d6d2a53bc9a183533e8409f837
Vusal <iamvusalalakbarov@gmail.com>
2026-01-22 13:41:40 +0400

    refactor: used size utility

--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -6,6 +6,6 @@ interface SpinnerProps {
 
 export const Spinner = ({ className }: SpinnerProps) => (
   <div className={clsx("flex items-center justify-center", className)}>
-    <div className="h-6 w-6 animate-spin rounded-full border-2 border-slate-700 border-t-cyan-500" />
+    <div className="size-6 animate-spin rounded-full border-2 border-slate-700 border-t-cyan-500" />
   </div>
 );
