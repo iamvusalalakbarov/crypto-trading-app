1e3b36713d9e41ce13d0eb6bb7f515c2c47efe7f
Vusal <iamvusalalakbarov@gmail.com>
2026-01-23 12:13:17 +0400

    feat: prefetched data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
-import { PropsWithChildren } from "react";
+import { JSX, PropsWithChildren } from "react";
 import { Roboto } from "next/font/google";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 import { ReactQueryProvider } from "@/providers/ReactQueryProvider";
 import { Header } from "@/components/Header";
+import { fetchAssets } from "@/lib/api";
 import "../styles/globals.css";
 
 const roboto = Roboto({
@@ -16,15 +22,26 @@ export const metadata: Metadata = {
     "The goal of this project is to use ReactJS capabilities and its paradigms by creating a simple Crypto Trading App.",
 };
 
-export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
+export default async function RootLayout({
+  children,
+}: Readonly<PropsWithChildren>): Promise<JSX.Element> {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["assets", "asc"],
+    queryFn: () => fetchAssets("asc"),
+  });
+
   return (
     <html lang="en">
       <body
         className={`${roboto.variable} text-slate-50 bg-slate-950 antialiased`}
       >
         <ReactQueryProvider>
-          <Header />
-          <main className="container mx-auto py-8">{children}</main>
+          <HydrationBoundary state={dehydrate(queryClient)}>
+            <Header />
+            <main className="container mx-auto py-8">{children}</main>
+          </HydrationBoundary>
         </ReactQueryProvider>
       </body>
     </html>
